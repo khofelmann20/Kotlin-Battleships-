38b63d72a6d36645c17709b275ed9a02358f88b8
dmc-account <55603701+dmc-account@users.noreply.github.com>
2020-01-14 11:21:57 -0800

    programmed basic kotlin battle ship

--- src/src/src/boardTest.kt
+++ src/src/src/boardTest.kt
@@ -1,9 +1,99 @@
 import java.util.Scanner
+import java.util.Random;
 fun main(){
-    // Creates an instance which takes input from standard input (keyboard)
     val reader = Scanner(System.`in`)
-    print("Enter a number: ");
-// nextInt() reads the next integer from the keyboard
-    var integer:Int = reader.nextInt();
-    println("You entered: $integer")
+//   val rand = ListOf Random.nextInt(0,10)
+    var shipX = arrayOf(1,2,3)
+    var shipY = arrayOf(1,1,1)
+    var ship2X = arrayOf(2,2,2)
+    var ship2Y = arrayOf(2,3,4)
+    var hit1 = 0
+    var miss1 = 0
+    var hit2 = 0
+    var miss2 = 0
+    var b = 0
+    var a = 0
+    var d = 0
+    var c = 0
+
+
+    while (b < 3 && d < 3) {
+        print("Player 1 x coordinate: ");
+        var integerx:Int = reader.nextInt();// nextInt() reads the next integer from the keyboard
+
+        print("Player 1 y coordinate: ")
+        var integery:Int = reader.nextInt()
+       
+        
+        if (shipX[0] == integerx) {
+            if (shipY[0] == integery) {
+                hit1++
+                a++
+            }
+        } else {
+            miss1++
+        }
+        if (shipX[1] == integerx) {
+            if (shipY[0] == integery) {
+                hit1++
+                a++
+            }
+        } else {
+            miss1++
+        }
+        if (shipX[2] == integerx) {
+            if (shipY[0] == integery) {
+                hit1++
+                a++
+            }
+        } else {
+            miss1++
+        }
+        if (hit1 > 0) {
+            print(" HIT ")
+        } else {
+            print(" MISS ")
+        }
+        print("Player 2 x coordinate: ");
+        var integer2x:Int = reader.nextInt();// nextInt() reads the next integer from the keyboard
+
+        print("Player 2 y coordinate: ")
+        var integer2y:Int = reader.nextInt()
+
+        if (ship2Y[0] == integer2y) {
+            if (ship2X[0] == integer2x) {
+                hit2++
+                c++
+            }
+        } else {
+            miss2++
+        }
+        if (ship2Y[1] == integer2y) {
+            if (ship2X[0] == integer2x) {
+                hit2++
+                c++
+            }
+        } else {
+            miss2++
+        }
+        if (ship2Y[2] == integer2y) {
+            if (ship2X[0] == integer2x) {
+                hit2++
+                c++
+            }
+        } else {
+            miss2++
+        }
+        if (hit2 > 0) {
+            print(" HIT ")
+        } else {
+            print(" MISS ")
+        }
+
+        b = a
+        d = c
+
+        hit1 = 0
+        hit2 = 0
+    }
 }
