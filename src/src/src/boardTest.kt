78ff66cc26c4e5c11baac5ffb728b34099510f5f
dmc-account <55603701+dmc-account@users.noreply.github.com>
2020-01-14 11:57:56 -0800

    I am going to add ascii art

--- src/src/src/boardTest.kt
+++ src/src/src/boardTest.kt
@@ -1,5 +1,7 @@
+import java.awt.Point
 import java.util.Scanner
 import java.util.Random;
+
 fun main(){
     val reader = Scanner(System.`in`)
 //   val rand = ListOf Random.nextInt(0,10)
@@ -15,85 +17,104 @@ fun main(){
     var a = 0
     var d = 0
     var c = 0
+    var boardX = arrayOf(1,2,3,4,5)
+    var boardY = arrayOf(1,2,3,4,5)
 
 
-    while (b < 3 && d < 3) {
-        print("Player 1 x coordinate: ");
-        var integerx:Int = reader.nextInt();// nextInt() reads the next integer from the keyboard
+//    val point = Point(1,3)
+//    println(point.x)
+//    print("Player 1 x coordinate: ")
+//    var integerx:Int = reader.nextInt()
+//    point.x = integerx
+//    println(point.x)
+  println("       _______________")
+    println("  8 |_|_|_|_|_|_|_|_|")
+    println("  7 |_|_|_|_|_|_|_|_|")
+    println("  6 |_|_|_|_|_|_|_|_|")
+    println("  5 |_|_|_|_|_|_|_|_|")
+    println("  4 |_|_|_|_|_|_|_|_|")
+    println("  3 |_|_|_|_|_|_|_|_|")
+    println("  2 |_|_|_|_|_|_|_|_|")
+    println("  1 |_|_|_|_|_|_|_|_|")
+    println("     1 2 3 4 5 6 7 8 ")
 
-        print("Player 1 y coordinate: ")
-        var integery:Int = reader.nextInt()
-       
-        
-        if (shipX[0] == integerx) {
-            if (shipY[0] == integery) {
-                hit1++
-                a++
-            }
-        } else {
-            miss1++
-        }
-        if (shipX[1] == integerx) {
-            if (shipY[0] == integery) {
-                hit1++
-                a++
-            }
-        } else {
-            miss1++
-        }
-        if (shipX[2] == integerx) {
-            if (shipY[0] == integery) {
-                hit1++
-                a++
-            }
-        } else {
-            miss1++
-        }
-        if (hit1 > 0) {
-            print(" HIT ")
-        } else {
-            print(" MISS ")
-        }
-        print("Player 2 x coordinate: ");
-        var integer2x:Int = reader.nextInt();// nextInt() reads the next integer from the keyboard
-
-        print("Player 2 y coordinate: ")
-        var integer2y:Int = reader.nextInt()
-
-        if (ship2Y[0] == integer2y) {
-            if (ship2X[0] == integer2x) {
-                hit2++
-                c++
-            }
-        } else {
-            miss2++
-        }
-        if (ship2Y[1] == integer2y) {
-            if (ship2X[0] == integer2x) {
-                hit2++
-                c++
-            }
-        } else {
-            miss2++
-        }
-        if (ship2Y[2] == integer2y) {
-            if (ship2X[0] == integer2x) {
-                hit2++
-                c++
-            }
-        } else {
-            miss2++
-        }
-        if (hit2 > 0) {
-            print(" HIT ")
-        } else {
-            print(" MISS ")
-        }
-
-        b = a
-        d = c
-
-        hit1 = 0
-        hit2 = 0
-    }
+//    while (b < 3 && d < 3) {
+//        print("Player 1 x coordinate: ");
+//        var integerx:Int = reader.nextInt();// nextInt() reads the next integer from the keyboard
+//
+//        print("Player 1 y coordinate: ")
+//        var integery:Int = reader.nextInt()
+//
+//
+//        if (shipX[0] == integerx) {
+//            if (shipY[0] == integery) {
+//                hit1++
+//                a++
+//            }
+//        } else {
+//            miss1++
+//        }
+//        if (shipX[1] == integerx) {
+//            if (shipY[0] == integery) {
+//                hit1++
+//                a++
+//            }
+//        } else {
+//            miss1++
+//        }
+//        if (shipX[2] == integerx) {
+//            if (shipY[0] == integery) {
+//                hit1++
+//                a++
+//            }
+//        } else {
+//            miss1++
+//        }
+//        if (hit1 > 0) {
+//            println(" HIT ")
+//        } else {
+//            println(" MISS ")
+//        }
+//        b = a
+//        print("Player 2 x coordinate: ");
+//        var integer2x:Int = reader.nextInt();// nextInt() reads the next integer from the keyboard
+//
+//        print("Player 2 y coordinate: ")
+//        var integer2y:Int = reader.nextInt()
+//
+//        if (ship2Y[0] == integer2y) {
+//            if (ship2X[0] == integer2x) {
+//                hit2++
+//                c++
+//            }
+//        } else {
+//            miss2++
+//        }
+//        if (ship2Y[1] == integer2y) {
+//            if (ship2X[0] == integer2x) {
+//                hit2++
+//                c++
+//            }
+//        } else {
+//            miss2++
+//        }
+//        if (ship2Y[2] == integer2y) {
+//            if (ship2X[0] == integer2x) {
+//                hit2++
+//                c++
+//            }
+//        } else {
+//            miss2++
+//        }
+//        if (hit2 > 0) {
+//            println(" HIT ")
+//        } else {
+//            println(" MISS ")
+//        }
+//
+//        d = c
+//
+//        hit1 = 0
+//        hit2 = 0
+//    }
 }
