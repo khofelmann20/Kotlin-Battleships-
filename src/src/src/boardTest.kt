3feb45f8627d07afd623ebb6e43ffaccb6846af1
dmc-account <55603701+dmc-account@users.noreply.github.com>
2020-01-14 10:19:22 -0800

    finally something works

---
+++ src/src/src/boardTest.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+fun main(){
+    // Creates an instance which takes input from standard input (keyboard)
+    val reader = Scanner(System.`in`)
+    print("Enter a number: ");
+// nextInt() reads the next integer from the keyboard
+    var integer:Int = reader.nextInt();
+    println("You entered: $integer")
+}
